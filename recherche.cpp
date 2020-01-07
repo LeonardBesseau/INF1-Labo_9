da39a0975e95d6ca4eb3b7f11f6ef3a9113ea871
leonard <leonard.besseau@heig-vd.ch>
2020-01-07 13:48:51 +0100

    added recherche linéaire

--- recherche.cpp
+++ recherche.cpp
@@ -13,3 +13,12 @@ Compilateur : g++ 7.4.0
 --------------------------- */
 
 #include "recherche.h"
+
+size_t rechercheLineaire(const std::vector<std::string> &dictionary, const std::string &word){
+    for (size_t i = 0; i < dictionary.size(); ++i) {
+        if(dictionary.at(i) == word){
+            return i;
+        }
+    }
+    return size_t(-1);
+}
--- recherche.h
+++ recherche.h
@@ -15,7 +15,13 @@ Compilateur : g++ 7.4.0
 #ifndef LABO_9_RECHERCHE_H
 #define LABO_9_RECHERCHE_H
 
+#include <vector>
+#include <string>
 
+size_t rechercheLineaire(const std::vector<std::string> &dictionary, const std::string &word);
+
+std::vector<std::string>::iterator rechercheLineaire(std::vector<std::string>::iterator begin,
+        std::vector<std::string>::iterator end , const std::string &word);
 
 
 

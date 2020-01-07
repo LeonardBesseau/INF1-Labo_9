5838d722a0727286adc1bec91f90d2233a2e450d
leonard <leonard.besseau@heig-vd.ch>
2020-01-07 19:35:45 +0100

    Added test for affichage

--- main.cpp
+++ main.cpp
@@ -20,7 +20,81 @@ Compilateur : g++ 7.4.0
 
 using namespace std;
 
+void testReadingFile() {
+    const vector<string> NATO{"Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India",
+                              "Juliet",
+                              "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango",
+                              "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu"};
+    vector<string> test = readFileByLine("/home/leonard/Downloads/nato.txt");
+    bool error = false;
+    for (int i = 0; i < NATO.size(); ++i) {
+        if (test.at(i) != NATO.at(i)) {
+            cout << "Error Inversion Test at index " << i << " element " << test.at(i) << " != " << NATO.at(i) << endl;
+            error = true;
+        }
+    }
+    cout << (!error ? "Reading from file OK " : "Error") << endl;
+}
+
+void testInversion() {
+    const vector<string> NATO{"Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India",
+                              "Juliet",
+                              "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango",
+                              "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu"};
+    const vector<string> OTAN{"Zulu", "Yankee", "Xray", "Whiskey", "Victor", "Uniform", "Tango", "Sierra", "Romeo",
+                              "Quebec",
+                              "Papa", "Oscar", "November", "Mike", "Lima", "Kilo", "Juliet", "India", "Hotel", "Golf",
+                              "Foxtrot", "Echo", "Delta", "Charlie", "Bravo", "Alpha"};
+
+    vector<string> v1 = NATO;
+    vector<string> v2 = OTAN;
+
+    inverseList(v1);
+    inverseList(v2);
+
+    bool error = false;
+
+    for (int i = 0; i < NATO.size(); ++i) {
+        if (v1.at(i) != OTAN.at(i)) {
+            cout << "Error Inversion Test at index " << i << " element " << v1.at(i) << " != " << OTAN.at(i) << endl;
+            error = true;
+        }
+        if (v2.at(i) != NATO.at(i)) {
+            cout << "Error Inversion Test at index " << i << " element " << v2.at(i) << " != " << NATO.at(i) << endl;
+            error = true;
+        }
+    }
+    cout << (!error ? "Inversion OK " : "Error") << endl;
+}
+
+void testBubbleSort() {
+    const vector<string> NATO_SHUFFLED{"kilo", "golf", "november", "charlie", "alpha", "sierra", "delta", "uniform",
+                                       "yankee", "tango", "echo", "lima", "xray", "victor", "bravo", "romeo", "zulu",
+                                       "hotel", "mike", "juliet", "quebec", "foxtrot", "india", "oscar", "papa",
+                                       "whiskey"};
+    const vector<string> NATO{"alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india",
+                              "juliet", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec", "romeo",
+                              "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu"};
+
+    vector<string> test = NATO_SHUFFLED;
+
+    bubbleSort(test);
+
+    bool error = false;
+    for (int i = 0; i < NATO.size(); ++i) {
+        if (test.at(i) != NATO.at(i)) {
+            cout << "Error Inversion Test at index " << i << " element " << test.at(i) << " != " << NATO.at(i) << endl;
+            error = true;
+        }
+    }
+    cout << (!error ? "BubbleSort OK " : "Error") << endl;
+}
+
 int main() {
+    testReadingFile();
+    testInversion();
+    testBubbleSort();
+    /*
     std::cout << "Hello, World!" << std::endl;
     vector<string> test{"Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliet",
                         "Kilo", "Lima", "Mike", "Novembe", "Osca", "Papa", "Quebec", "Romeo", "Sierra", "Tango",
@@ -33,12 +107,14 @@ int main() {
     inverseList(test);
     string a = "Whiskey";
     for (int i = 0; i < test.size(); ++i) {
-        cout <<test_shuffled.at(i)<<" "<< rechercheLineaire(test_shuffled, test_shuffled.at(i))<<endl;
+        cout << test_shuffled.at(i) << " " << rechercheLineaire(test_shuffled, test_shuffled.at(i)) << endl;
     }
     cout << endl;
     for (int i = 0; i < test.size(); ++i) {
-        cout <<test_shuffled.at(i)<<" "<< 26-(test_shuffled.end()-rechercheLineaire(test_shuffled.begin(), test_shuffled.end(), test_shuffled.at(i)))<<endl;
+        cout << test_shuffled.at(i) << " " << 26 - (test_shuffled.end() -
+                                                    rechercheLineaire(test_shuffled.begin(), test_shuffled.end(),
+                                                                      test_shuffled.at(i))) << endl;
     }
-
+*/
     return 0;
 }
